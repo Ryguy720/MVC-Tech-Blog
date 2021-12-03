f8db0d45170c15cd410d52fa0cb841a95507157c
unknown <mccullochryan636@gmail.com>
2021-12-02 18:21:27 -0700

    added blog.js

--- public/js/login.js
+++ public/js/login.js
@@ -28,6 +28,7 @@ const signupFormHandler = async (event) => {
   const name = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+  console.log(name,email,password)
 
   if (name && email && password) {
     const response = await fetch('/api/users', {
@@ -35,7 +36,7 @@ const signupFormHandler = async (event) => {
       body: JSON.stringify({ name, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
-
+     console.log(response)
     if (response.ok) {
       document.location.replace('/profile');
     } else {
